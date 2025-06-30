a4fa33027e92223c90344cb47ebc7b31135442cf
tiago-silva-batista <tiagosbsantos@outlook.comm>
2025-06-30 17:48:07 -0300

    Deploy direto da raiz para a Vercel

--- gulpfile.js
+++ gulpfile.js
@@ -7,51 +7,48 @@ const browserSync = require('browser-sync').create();
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css'))
-        .pipe(browserSync.stream()); // recarrega só o CSS
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
 }
 
-// Otimizar imagens
+// Imagens
 function images() {
     return gulp.src('./assets/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/assets/images'));
+        .pipe(gulp.dest('./assets/images'));
 }
 
-// Copiar ícones
+// Ícones
 function icons() {
     return gulp.src('./assets/icons/**/*')
-        .pipe(gulp.dest('./dist/assets/icons'));
+        .pipe(gulp.dest('./assets/icons'));
 }
 
-// Copiar PDF
+// PDFs
 function pdf() {
     return gulp.src('./assets/pdf/**/*')
-        .pipe(gulp.dest('./dist/assets/pdf'));
+        .pipe(gulp.dest('./assets/pdf'));
 }
 
-// Copiar HTML
+// HTML
 function html() {
     return gulp.src('./index.html')
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./'));
 }
 
 // Servidor local
 function serve() {
     browserSync.init({
         server: {
-            baseDir: './dist'
+            baseDir: './'
         },
         port: 3000
     });
 
     gulp.watch('./src/styles/*.scss', styles);
     gulp.watch('./assets/images/**/*', gulp.series(images, browserSync.reload));
-    gulp.watch('./assets/icons/**/*', gulp.series(icons, browserSync.reload));
-    gulp.watch('./assets/pdf/**/*', gulp.series(pdf, browserSync.reload));
     gulp.watch('./index.html', gulp.series(html, browserSync.reload));
 }
 
-// Task padrão
 exports.default = gulp.parallel(styles, images, icons, pdf, html);
 exports.serve = gulp.series(styles, images, icons, pdf, html, serve);
