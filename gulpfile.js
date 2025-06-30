2ec7cdf6e647d9e10cd737f794d759b8baf2d649
tiago-silva-batista <tiagosbsantos@outlook.comm>
2025-06-30 18:08:19 -0300

    Corrigido build para pasta public com Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,32 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
-const browserSync = require('browser-sync').create();
 
-// Compilar SCSS
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest('./public/css'));
 }
 
-// Imagens
 function images() {
     return gulp.src('./assets/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./assets/images'));
+        .pipe(gulp.dest('./public/assets/images'));
 }
 
-// Ícones
 function icons() {
     return gulp.src('./assets/icons/**/*')
-        .pipe(gulp.dest('./assets/icons'));
+        .pipe(gulp.dest('./public/assets/icons'));
 }
 
-// PDFs
 function pdf() {
     return gulp.src('./assets/pdf/**/*')
-        .pipe(gulp.dest('./assets/pdf'));
+        .pipe(gulp.dest('./public/assets/pdf'));
 }
 
-// HTML
 function html() {
     return gulp.src('./index.html')
-        .pipe(gulp.dest('./'));
-}
-
-// Servidor local
-function serve() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        },
-        port: 3000
-    });
-
-    gulp.watch('./src/styles/*.scss', styles);
-    gulp.watch('./assets/images/**/*', gulp.series(images, browserSync.reload));
-    gulp.watch('./index.html', gulp.series(html, browserSync.reload));
+        .pipe(gulp.dest('./public'));
 }
 
 exports.default = gulp.parallel(styles, images, icons, pdf, html);
-exports.serve = gulp.series(styles, images, icons, pdf, html, serve);
