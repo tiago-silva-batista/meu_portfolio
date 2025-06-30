994adb31f4ff0c3147bf13df1c3a31a58132dab9
tiago-silva-batista <tiagosbsantos@outlook.comm>
2025-06-30 17:24:13 -0300

    Forçando rebuild completo com dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,14 @@
-// const gulp = require('gulp');
-// const sass = require('gulp-sass')(require('sass'));
-// const imagemin = require('gulp-imagemin');
-
-// function styles() {
-//     return gulp.src('./src/styles/*.scss')
-//         .pipe(sass({ ouputStyle: 'compressed' }))
-//         .pipe(gulp.dest('./dist/css'));
-// }
-
-// function images() {
-//     return gulp.src('./assets/images/**/*')
-//         .pipe(imagemin())
-//         .pipe(gulp.dest('./dist/images'));
-// }
-
-// function html() {
-//     return gulp.src('./src/*.html') // ou onde estiver seu HTML
-//         .pipe(gulp.dest('./dist'));
-// }
-
-// exports.default = gulp.parallel(styles, images, html);
-// exports.watch = function () {
-//     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-// }
-
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
 // Compilar SCSS
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream()); // recarrega só o CSS
 }
 
 // Otimizar imagens
@@ -42,17 +18,40 @@ function images() {
         .pipe(gulp.dest('./dist/assets/images'));
 }
 
-// Copiar ícones e outros assets
-function assets() {
+// Copiar ícones
+function icons() {
     return gulp.src('./assets/icons/**/*')
         .pipe(gulp.dest('./dist/assets/icons'));
 }
 
-// Mover HTML para a pasta final
+// Copiar PDF
+function pdf() {
+    return gulp.src('./assets/pdf/**/*')
+        .pipe(gulp.dest('./dist/assets/pdf'));
+}
+
+// Copiar HTML
 function html() {
     return gulp.src('./index.html')
         .pipe(gulp.dest('./dist'));
 }
 
-// Task default
-exports.default = gulp.parallel(styles, images, assets, html,);
+// Servidor local
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        },
+        port: 3000
+    });
+
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./assets/images/**/*', gulp.series(images, browserSync.reload));
+    gulp.watch('./assets/icons/**/*', gulp.series(icons, browserSync.reload));
+    gulp.watch('./assets/pdf/**/*', gulp.series(pdf, browserSync.reload));
+    gulp.watch('./index.html', gulp.series(html, browserSync.reload));
+}
+
+// Task padrão
+exports.default = gulp.parallel(styles, images, icons, pdf, html);
+exports.serve = gulp.series(styles, images, icons, pdf, html, serve);
