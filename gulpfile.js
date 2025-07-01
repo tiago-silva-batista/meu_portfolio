cf84ac164f5a308fce4e4aff238d8ded437ca0e6
tiago-silva-batista <tiagosbsantos@outlook.comm>
2025-07-01 17:21:33 -0300

    corrigindo o header

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,70 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
+// Compila SCSS
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./public/css'));
+        .pipe(gulp.dest('./public/css'))
+        .pipe(browserSync.stream()); // injeta o CSS sem recarregar a página
 }
 
+// Otimiza imagens
 function images() {
     return gulp.src('./assets/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./public/assets/images'));
 }
 
+// Copias as fontes
+function fonts() {
+    return gulp.src('assets/fonts/**/*')
+        .pipe(gulp.dest('public/assets/fonts'));
+}
+
+
+// Copia ícones
 function icons() {
     return gulp.src('./assets/icons/**/*')
         .pipe(gulp.dest('./public/assets/icons'));
 }
 
+// Copia arquivos PDF
 function pdf() {
     return gulp.src('./assets/pdf/**/*')
         .pipe(gulp.dest('./public/assets/pdf'));
 }
 
+// Copia HTML
 function html() {
     return gulp.src('./index.html')
         .pipe(gulp.dest('./public'));
 }
 
-exports.default = gulp.parallel(styles, images, icons, pdf, html);
+// Inicia servidor com hot reload
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: './public'
+        },
+        notify: false, // remove "BrowserSync is connected"
+        ui: false      // remove painel extra na porta 3001
+    });
+
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./index.html', html).on('change', browserSync.reload);
+    gulp.watch('./assets/images/**/*', images).on('change', browserSync.reload);
+    gulp.watch('./assets/icons/**/*', icons).on('change', browserSync.reload);
+    gulp.watch('./assets/pdf/**/*', pdf).on('change', browserSync.reload);
+}
+
+// Task padrão (compila tudo)
+exports.default = gulp.parallel(styles, images, icons, pdf, html, fonts);
+
+// Task para desenvolvimento com hot reload
+exports.serve = gulp.series(
+    gulp.parallel(styles, images, icons, pdf, html, fonts),
+    serve
+);
